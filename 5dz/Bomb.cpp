1b3d96cade9687a389ed56ab119f8cc6f73d28bf
IvanYunkevich <ivan01089@mail.ru>
2023-02-18 17:17:01 +0300

    5dz

---
+++ 5dz/Bomb.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "Bomb.h"
+#include "MyTools.h"
+#include "DestroyableGroundObject.h"
+
+using namespace std;
+using namespace MyTools;
+
+void Bomb::Draw() const
+{
+    SetColor(CC_LightMagenta);
+    GotoXY(x, y);
+    cout << "*";
+}
+
+void __fastcall Bomb::Accept(const Visitor& v)
+{
+    v.log(*this);
+}
+
+
+
+DestroyableGroundObject* Bomb::CheckDestoyableObjects()
+{
+    for (DestroyableGroundObject* obs : obsVec)
+    {
+        const double x1 = x - width / 2.0;
+        const double x2 = x1 + width;
+
+        if (obs->isInside(x1, x2)) return obs;
+    }
+
+    return nullptr;
+}
+
+void Bomb::addObservers(DestroyableGroundObject* obj)
+{
+    obsVec.push_back(obj);
+}
---
+++ 5dz/Bomb.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "DynamicObject.h"
+#include <vector>
+
+class DestroyableGroundObject;
+
+class Bomb : public DynamicObject
+{
+public:
+
+	static const uint16_t BombCost = 10; // ñòîèìîñòü áîìáû â î÷êàõ
+
+	void Draw() const override;
+
+	void Accept(const Visitor& v) override;
+
+	DestroyableGroundObject* CheckDestoyableObjects();
+
+	void addObservers(DestroyableGroundObject* obj);
+
+private:
+
+	std::vector<DestroyableGroundObject*> obsVec;
+
+};
